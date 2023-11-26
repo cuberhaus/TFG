61c65992a03f512b082a1e0ed2fe4403f7835fd0
Pol Casacuberta <polcg10@gmail.com>
2023-11-26 11:07:19 +0100

    train models pass model as parameter, not hardcoded

--- code/train_models.sh
+++ code/train_models.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# Check if a JSON file path is provided as an argument
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 path_to_json_file"
+    exit 1
+fi
+
+# Assign the first argument to JSON_FILE variable
+JSON_FILE="$1"
+
+# Check if the JSON file exists
+if [ ! -f "$JSON_FILE" ]; then
+    echo "Error: JSON file not found at $JSON_FILE"
+    exit 1
+fi
+
 # Assuming the JSON file is named 'model_configs.json'
-JSON_FILE="model_configs.json"
+#JSON_FILE="model_configs.json"
 
 # Read and loop through each entry in the JSON file
 for row in $(cat $JSON_FILE | jq -c '.[]'); do
