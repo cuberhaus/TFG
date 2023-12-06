375c7399c068e7f22fff206fb8e27f8b47bed46c
cuberhaus <polcg10@gmail.com>
2023-12-06 13:54:49 +0100

    transfer multiple directories

--- code/src/shell_scripts/transfer_remote.sh
+++ code/src/shell_scripts/transfer_remote.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Define remote host and directory
+# Define remote host and base directory
 REMOTE_HOST="casacuberta@teegarden.cs.upc.edu"
 REMOTE_DIR="/home/casacuberta/TFG/code/"
 
-# Define the local directory to be transferred
-LOCAL_DIR="./src/"
+# Define the local directories to be transferred
+LOCAL_DIRS=("./src/" "./configurations/")
 
-# Transfer the entire directory
-echo "Transferring $LOCAL_DIR to $REMOTE_HOST:$REMOTE_DIR"
-scp -r "$LOCAL_DIR" "$REMOTE_HOST":"$REMOTE_DIR"
-
-echo "All files in $LOCAL_DIR transferred to $REMOTE_HOST:$REMOTE_DIR"
+# Loop over each local directory and transfer it
+for DIR in "${LOCAL_DIRS[@]}"; do
+    echo "Transferring $DIR to $REMOTE_HOST:$REMOTE_DIR"
+    scp -r "$DIR" "$REMOTE_HOST":"$REMOTE_DIR"
+    echo "All files in $DIR transferred to $REMOTE_HOST:$REMOTE_DIR"
+done
