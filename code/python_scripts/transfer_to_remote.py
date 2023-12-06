96636ed495fb062161ad90f4ba0e8846a3c99c59
cuberhaus <polcg10@gmail.com>
2023-12-06 12:01:14 +0100

    This is a placeholder

--- code/python_scripts/transfer_to_remote.py
+++ code/python_scripts/transfer_to_remote.py
@@ -13,22 +13,32 @@ remote_dir = "/home/casacuberta/TFG/code/"
 files = [
     "./custom_dataset.py",
     "./model_utils.py",
-    "./test_model.py",
     "./optuna_train_model.py",
     "./raytune_train_model.py",
     "./train_and_save_model.py",
-    "./train_models.sh",
-    "./model.json"
+
+    "./python_scripts/test_model.py",
+    "./shell_scripts/train_models.sh",
+    "./configurations/model.json"
 ]
 
 # Loop through each file and transfer it
 for file in files:
+    # Extract the directory path relative to the root directory
+    relative_dir = os.path.dirname(file)
+    remote_path = os.path.join(remote_dir, relative_dir)
+
+    # Create the corresponding directory on the remote server
+    if relative_dir:  # Check if the file is not in the root directory
+        subprocess.run(["ssh", remote_host, f"mkdir -p {remote_path}"])
+
     # Extract the base filename
     basefile = os.path.basename(file)
-    print(f"Transferring {file} to {remote_host}:{remote_dir}{basefile}")
+    full_remote_path = os.path.join(remote_path, basefile)
+    print(f"Transferring {file} to {remote_host}:{full_remote_path}")
 
-    # Execute scp command
-    scp_command = ["scp", file, f"{remote_host}:{remote_dir}{basefile}"]
+    # Execute scp command to transfer the file
+    scp_command = ["scp", file, f"{remote_host}:{full_remote_path}"]
     subprocess.run(scp_command)
 
 print(f"All files transferred to {remote_host}:{remote_dir}")
