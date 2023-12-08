bfe8a6f1d31f8e8e1be5d572c079c9dbe8cdee8b
cuberhaus <polcg10@gmail.com>
2023-12-08 12:08:45 +0100

    fixed?

--- code/src/cyclegan_test.py
+++ code/src/cyclegan_test.py
@@ -4,7 +4,7 @@ import subprocess
 from clases.cyclegan import prepareCycleGAN
 
 POLYP_DATASET_DIR, python_installed, = prepareCycleGAN()
-test_dataset_dir = os.path.join(POLYP_DATASET_DIR, 'testA')
-print(test_dataset_dir)
+# test_dataset_dir = os.path.join(POLYP_DATASET_DIR, 'testA')
+# print(test_dataset_dir)
 subprocess.run(
-    [python_installed, 'test.py', '--dataroot', test_dataset_dir, '--name', 'mask2polyp', '--model', 'test', '--no_dropout'])
+    [python_installed, 'test.py', '--dataroot', POLYP_DATASET_DIR, '--name', 'mask2polyp', '--model', 'cycle_gan', '--no_dropout'])
