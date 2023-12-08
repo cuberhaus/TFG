eada6fc091767b99e2b7743778c96fe3c2fed984
cuberhaus <polcg10@gmail.com>
2023-12-08 11:59:05 +0100

    This is a placeholder

--- code/src/cyclegan_test.py
+++ code/src/cyclegan_test.py
@@ -5,5 +5,6 @@ from clases.cyclegan import prepareCycleGAN
 
 python_installed, POLYP_DATASET_DIR = prepareCycleGAN()
 test_dataset_dir = os.path.join(POLYP_DATASET_DIR, 'testA')
+print(test_dataset_dir)
 subprocess.run(
     [python_installed, 'test.py', '--dataroot', test_dataset_dir, '--name', 'mask2polyp', '--model', 'test', '--no_dropout'])
