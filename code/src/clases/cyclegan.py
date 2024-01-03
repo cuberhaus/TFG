0d27ce96a2b5df7f4361e9f02ffcb1b877b3e7da
cuberhaus <polcg10@gmail.com>
2024-01-03 17:43:03 +0100

    This is a placeholder

--- code/src/clases/cyclegan.py
+++ code/src/clases/cyclegan.py
@@ -5,18 +5,16 @@ from clases.utils import python_version
 
 
 def prepareCycleGAN():
-    # Script dir
-    clases_dir = os.path.dirname(__file__)  # Directory of the script file
-    src_dir = os.path.join(clases_dir, "../")  # Full path to the file
-    cycleGan_dir = os.path.join(src_dir, "../tmp/pytorch-CycleGAN-and-pix2pix")  # Full path to the file
-    # check if the directory exists
+    clases_dir = os.path.dirname(__file__)
+    src_dir = os.path.join(clases_dir, "../")
+    cycleGan_dir = os.path.join(src_dir, "../tmp/pytorch-CycleGAN-and-pix2pix")
+
     if not os.path.exists(cycleGan_dir):
         subprocess.run(['git', 'clone', 'https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix', cycleGan_dir])
         requirements_dir = os.path.join(cycleGan_dir, 'requirements.txt')
         subprocess.run(['pip', 'install', '-r', requirements_dir])
     os.chdir(cycleGan_dir)
     POLYP_DATASET_DIR = os.path.join(src_dir, "../data/PolypDataset")
-    # PYTHON_INSTALLED = is_python_installed()
     python_installed = python_version()
     print(POLYP_DATASET_DIR)
     print(python_installed)
--- code/src/clases/utils.py
+++ code/src/clases/utils.py
@@ -21,9 +21,7 @@ def python_version():
 
 
 def is_python_installed():
-    # Check for Python (may be Python 2)
     python_installed = check_python_version("python")
-    # Check for Python 3
     python3_installed = check_python_version("python3")
     if python3_installed:
         return PYTHON_INSTALLED.PYTHON_3
@@ -35,15 +33,11 @@ def is_python_installed():
 
 def check_python_version(version):
     try:
-        # Run the command to check for Python version
         subprocess.run([version, "--version"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        # print(f"{version} is installed.")
         return True
     except subprocess.CalledProcessError:
-        # print(f"{version} is not installed.")
         return False
     except FileNotFoundError:
-        # print(f"{version} is not installed.")
         return False
     except Exception as e:
         print(f"Error: {e}")
@@ -52,18 +46,14 @@ def check_python_version(version):
 
 def is_wsl():
     """
-    Check if the current system is Windows Subsystem for Linux (WSL).
-    :return:
+        Check if using Windows Subsystem for Linux
     """
-    # Check the platform identifier
     if platform.system() != 'Linux':
         return False
 
-    # Read the contents of /proc/version
     try:
         with open('/proc/version', 'r') as f:
             content = f.read().lower()
-            # Look for keywords indicating WSL
             return 'microsoft' in content or 'wsl' in content
     except FileNotFoundError:
         return False
