ac92942bba162c7b7f5c0ff5c6cb55f6ad1acf81
cuberhaus <polcg10@gmail.com>
2024-01-03 17:03:43 +0100

    less comments

--- code/src/python_scripts/copy_files_to_cyclegan_structure.py
+++ code/src/python_scripts/copy_files_to_cyclegan_structure.py
@@ -16,31 +16,21 @@ This script copies the files from the PolypDetection dataset to the CycleGAN str
 #         └── testB   # Images for testing
 
 def copy_files_to_cyclegan_structure(root_dir, target_dir, mode='train'):
-    """
-    Copy files from the PolypDetection dataset to the CycleGAN structure.
-    :param root_dir:
-    :param target_dir:
-    :param mode:
-    :return:
-    """
     assert mode in ('train', 'test'), "Mode must be 'train' or 'test'."
-    # Define the source directories
+
     images_root_dir = Path(root_dir) / 'Images'
     masks_root_dir = Path(root_dir) / 'Masks'
 
-    # Set up train or test directories based on the mode
     trainA_dir = Path(target_dir) / f'{mode}A'
     trainB_dir = Path(target_dir) / f'{mode}B'
 
-    # Create the target directories if they don't exist
     trainA_dir.mkdir(parents=True, exist_ok=True)
     trainB_dir.mkdir(parents=True, exist_ok=True)
 
-    # Copy masks to trainA and images to trainB
     print("Copying files to CycleGAN structure...")
     for video_number in sorted(images_root_dir.iterdir()):
         print(f"Processing video {video_number}")
-        if video_number.is_dir():  # Check if it is a directory
+        if video_number.is_dir():
             mask_subdir = masks_root_dir / video_number.name
             if not mask_subdir.exists() or not mask_subdir.is_dir():
                 print(f"Mask directory does not exist: {mask_subdir}")
@@ -51,7 +41,6 @@ def copy_files_to_cyclegan_structure(root_dir, target_dir, mode='train'):
                 print(f"Image directory does not exist: {image_subdir}")
                 continue
 
-            # Copy mask files to trainA
             mask_files = list(mask_subdir.glob('*.png'))
             if not mask_files:
                 print(f"No mask files found in directory: {mask_subdir}")
@@ -60,9 +49,7 @@ def copy_files_to_cyclegan_structure(root_dir, target_dir, mode='train'):
             for mask_file in mask_files:
                 new_mask_filename = f"{video_number.name}_{mask_file.name}"
                 shutil.copy(mask_file, trainA_dir / new_mask_filename)
-                # print(f"Copied {mask_file} to {trainA_dir / new_mask_filename}")
 
-            # Copy image files to trainB
             image_files = list(image_subdir.glob('*.jpg'))
             if not image_files:
                 print(f"No image files found in directory: {image_subdir}")
@@ -71,12 +58,10 @@ def copy_files_to_cyclegan_structure(root_dir, target_dir, mode='train'):
             for image_file in image_files:
                 new_image_filename = f"{video_number.name}_{image_file.name}"
                 shutil.copy(image_file, trainB_dir / new_image_filename)
-                # print(f"Copied {image_file} to {trainB_dir / new_image_filename}")
 
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Example usage
 root_dir = os.path.join(SCRIPT_DIR, '../../data/TrainValid/TrainValid/')
 target_dir = os.path.join(SCRIPT_DIR, '../../data/PolypDataset/')
 copy_files_to_cyclegan_structure(root_dir, target_dir, mode='train')
