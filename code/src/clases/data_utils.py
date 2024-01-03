255c4598dc75d6182cea3b1038a3e4f9a0172d26
cuberhaus <polcg10@gmail.com>
2024-01-03 17:19:47 +0100

    less comments

--- code/src/clases/data_utils.py
+++ code/src/clases/data_utils.py
@@ -7,9 +7,6 @@ import pandas as pd
 
 
 def get_all_bounding_boxes(dataset):
-    """
-    Get all bounding boxes from a dataset.
-    """
     all_bounding_boxes = []
     for _, target in dataset:
         boxes = target['boxes'].cpu().numpy()
@@ -19,9 +16,6 @@ def get_all_bounding_boxes(dataset):
 
 
 def calculate_wcss(data, max_k=10):
-    """
-    Calculate the within-cluster sum of squares (WCSS) for different numbers of clusters.
-    """
     wcss = []
     for k in range(1, max_k + 1):
         kmeans = KMeans(n_clusters=k, init='k-means++', max_iter=300, n_init=10, random_state=0)
@@ -31,9 +25,6 @@ def calculate_wcss(data, max_k=10):
 
 
 def plot_elbow(wcss):
-    """
-    Plot the within-cluster sum of squares (WCSS) for different numbers of clusters.
-    """
     plt.figure(figsize=(10, 8))
     plt.plot(range(1, len(wcss) + 1), wcss, marker='o')
     plt.title('The Elbow Method')
@@ -45,18 +36,8 @@ def plot_elbow(wcss):
 
 
 def calculate_silhouette_scores(data, max_k=10):
-    """
-    Calculate silhouette scores for different numbers of clusters.
-
-    Parameters:
-    - data: The data used for clustering.
-    - max_k: Maximum number of clusters to try.
-
-    Returns:
-    - A list of silhouette scores corresponding to the number of clusters.
-    """
     silhouette_scores = []
-    for k in range(2, max_k + 1):  # Silhouette score is only defined for 2 or more clusters
+    for k in range(2, max_k + 1):  # Silhouette score only makes sense for 2 or more clusters
         kmeans = KMeans(n_clusters=k, init='k-means++', max_iter=300, n_init=10, random_state=0)
         cluster_labels = kmeans.fit_predict(data)
         silhouette_avg = silhouette_score(data, cluster_labels)
@@ -65,12 +46,6 @@ def calculate_silhouette_scores(data, max_k=10):
 
 
 def plot_silhouette_scores(silhouette_scores):
-    """
-    Plot the silhouette scores for different numbers of clusters.
-
-    Parameters:
-    - silhouette_scores: List of silhouette scores.
-    """
     plt.figure(figsize=(10, 8))
     plt.plot(range(2, len(silhouette_scores) + 2), silhouette_scores, marker='o')
     plt.title('Silhouette Scores for Different Numbers of Clusters')
@@ -82,15 +57,6 @@ def plot_silhouette_scores(silhouette_scores):
 
 
 def get_bbox_centers(bounding_boxes):
-    """
-    Calculate the centers of bounding boxes.
-
-    Parameters:
-    - bounding_boxes: A list of bounding boxes, each represented as [x_min, y_min, x_max, y_max].
-
-    Returns:
-    - A list of centers for each bounding box, each represented as [x_center, y_center].
-    """
     centers = []
     for bbox in bounding_boxes:
         x_min, y_min, x_max, y_max = bbox
@@ -101,27 +67,13 @@ def get_bbox_centers(bounding_boxes):
 
 
 def cluster_bounding_boxes(bbox_centers, n_clusters=3):
-    """
-    Cluster bounding boxes using K-means clustering.
-    """
     bbox_centers = np.array(bbox_centers)
-    # Apply K-means clustering
     kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(bbox_centers)
-
-    # Get cluster centers
     centers = kmeans.cluster_centers_
-
     return centers, kmeans.labels_
 
 
 def plot_cluster_centers(centers):
-    """
-    Plot the cluster centers on a chart.
-
-    Parameters:
-    - centers: numpy array of cluster centers with shape (M, 2), where M is the number of centers,
-      and each center is defined by (x_center, y_center).
-    """
     plt.figure(figsize=(10, 8))
     plt.scatter(centers[:, 0], centers[:, 1], c='red', marker='x', label='Cluster Centers')
 
@@ -137,20 +89,13 @@ def plot_cluster_centers(centers):
 
 
 def plot_cluster_centers_with_bbox_centers(centers, bbox_centers):
-    """
-    Plot the cluster centers and the centers of the bounding boxes on a chart.
-    """
-
-    # Convert lists to numpy arrays if they are not already
     if isinstance(bbox_centers, list):
         bbox_centers = np.array(bbox_centers)
     if isinstance(centers, list):
         centers = np.array(centers)
 
-    # Plot the centers of the bounding boxes
     plt.scatter(bbox_centers[:, 0], bbox_centers[:, 1], c='blue', marker='o', label='Bounding Box Centers')
 
-    # Plot the cluster centers
     plt.scatter(centers[:, 0], centers[:, 1], c='red', marker='x', label='Cluster Centers')
 
     # Annotate the cluster centers
@@ -166,30 +111,21 @@ def plot_cluster_centers_with_bbox_centers(centers, bbox_centers):
 
 
 def plot_density_of_bbox_centers(bbox_centers):
-    """
-    Generate a density plot of the centers of bounding boxes with a legend.
-
-    Parameters:
-    - bounding_boxes: A numpy array or a list of bounding boxes with each box defined as [x_min, y_min, x_max, y_max].
-    """
-    # Convert bounding boxes to centers
     if isinstance(bbox_centers, list):
         bbox_centers = np.array(bbox_centers)
 
-    # Convert to DataFrame for Seaborn
+    # Needed for Seaborn
     df_centers = pd.DataFrame(bbox_centers, columns=['x_center', 'y_center'])
 
-    # Create the density plot
     plt.figure(figsize=(10, 8))
-    # ax = plt.gca()
     ax = sns.kdeplot(data=df_centers, x='x_center', y='y_center', fill=True, cmap="Blues")
 
-    # Add a color bar representing the density scale
+    # Create a color bar for the scale
     norm = plt.Normalize(df_centers.values.min(), df_centers.values.max())
     sm = plt.cm.ScalarMappable(cmap="Blues", norm=norm)
     sm.set_array([])
 
-    # Create a color bar as the legend
+    # Add a color bar as the legend
     color_bar = plt.colorbar(sm, ax=ax)
     color_bar.set_label('Density')
 
@@ -199,34 +135,22 @@ def plot_density_of_bbox_centers(bbox_centers):
 
 
 def plot_density_of_bbox_centers_with_clusters(bbox_centers, cluster_centers):
-    """
-    Generate a density plot of the centers of bounding boxes with cluster centers.
-
-    Parameters:
-    - bbox_centers: A numpy array or a list of bounding box centers with each center defined as [x_center, y_center].
-    - cluster_centers: A numpy array or a list of cluster centers.
-    """
-    # Convert bounding box centers to numpy array if they are not already
     if isinstance(bbox_centers, list):
         bbox_centers = np.array(bbox_centers)
 
-    # Convert to DataFrame for Seaborn
+    # Needed for Seaborn
     df_centers = pd.DataFrame(bbox_centers, columns=['x_center', 'y_center'])
 
-    # Create the density plot
     plt.figure(figsize=(10, 8))
     ax = sns.kdeplot(data=df_centers, x='x_center', y='y_center', fill=True, cmap="Blues", legend=True)
 
-    # Add a color bar representing the density scale
     norm = plt.Normalize(df_centers.values.min(), df_centers.values.max())
     sm = plt.cm.ScalarMappable(cmap="Blues", norm=norm)
     sm.set_array([])
 
-    # Create a color bar as the legend
     color_bar = plt.colorbar(sm,ax=ax)
     color_bar.set_label('Density')
 
-    # Plot the cluster centers
     if isinstance(cluster_centers, list):
         cluster_centers = np.array(cluster_centers)
     plt.scatter(cluster_centers[:, 0], cluster_centers[:, 1], c='red', marker='x', label='Cluster Centers')
