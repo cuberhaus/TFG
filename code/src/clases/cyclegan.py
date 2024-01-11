ae8b9031b3e3dbb601af6a2856842d6b94559e25
cuberhaus <polcg10@gmail.com>
2024-01-11 13:50:21 +0100

    spade works

--- code/src/clases/cyclegan.py
+++ code/src/clases/cyclegan.py
@@ -24,12 +24,23 @@ def prepareSPADE():
     clases_dir = os.path.dirname(__file__)
     src_dir = os.path.join(clases_dir, "../")
     cycleGan_dir = os.path.join(src_dir, "../tmp/SPADE")
+    extra_dir = os.path.join(cycleGan_dir, "models/networks/Synchronized-BatchNorm-PyTorch")
+    copy_from_dir = os.path.join(cycleGan_dir, "models/networks/Synchronized-BatchNorm-PyTorch/sync_batchnorm")
+    copy_to_dir = os.path.join(cycleGan_dir, "models/networks/sync_batchnorm")
+    print(copy_from_dir)
+    print(copy_to_dir)
 
     if not os.path.exists(cycleGan_dir):
         subprocess.run(['git', 'clone', 'https://github.com/NVlabs/SPADE.git', cycleGan_dir])
         requirements_dir = os.path.join(cycleGan_dir, 'requirements.txt')
         subprocess.run(['pip', 'install', '-r', requirements_dir])
-    os.chdir(cycleGan_dir)
+        subprocess.run(['git', 'clone', 'https://github.com/vacancy/Synchronized-BatchNorm-PyTorch', extra_dir])
+    # subprocess.run(['cp', '-rf', 'Synchronized-BatchNorm-PyTorch/sync_batchnorm', extra_dir])
+        subprocess.run(['cp', "-rf", copy_from_dir, copy_to_dir])  # in powershell -Recurse -Force
+
+
+    # cp -rf Synchronized-BatchNorm-PyTorch/sync_batchnorm .
+    # cd ../../
     POLYP_DATASET_DIR = os.path.join(src_dir, "../data/PolypDataset")
     python_installed = python_version()
     print(POLYP_DATASET_DIR)
--- code/src/spade_train.py
+++ code/src/spade_train.py
@@ -1,3 +1,19 @@
+import os
+import subprocess
+
 from clases.cyclegan import prepareSPADE
 
-prepareSPADE()
+POLYP_DATASET_DIR, python_installed, cycleGan_dir = prepareSPADE()
+testA_dir = os.path.join(POLYP_DATASET_DIR, "testA")  # masks
+testB_dir = os.path.join(POLYP_DATASET_DIR, "testB")  # images
+
+train_dir = os.path.join(cycleGan_dir, "train.py")
+
+command = (["python", train_dir, "--name", "spade_train", "--dataset_mode", "custom", "--label_dir", testA_dir,
+            "--image_dir", testB_dir, "--label_nc", "2"])
+print(command)
+
+# To train on your own custom dataset python train.py --name [experiment_name] --dataset_mode custom --label_dir [
+# path_to_labels] -- image_dir [path_to_images] --label_nc [num_labels]
+
+subprocess.run(command)
